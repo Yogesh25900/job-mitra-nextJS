58d8fe3ed280f9be5d499cf928b381f8d3230799
yogesh25900 <chaudharyyogesh258@gmail.com>
2026-01-31 20:18:58 +0545

    update admin api endpoint

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -1,14 +1,28 @@
-
-
 export const API = {
     AUTH: {
         TALENT: {
-            LOGIN: '/api/talent/login',
-            REGISTER: '/api/talent/register',
+            LOGIN: '/api/talentusers/login',
+            REGISTER: '/api/talentusers/register',
+            EDITPROFILE: '/api/talentusers',
+            GETPROFILEBYID: '/api/talentusers',
+            GETPROFILEMYSELF: '/api/talentusers/profile/me',
+            UPLOADPHOTO: '/api/talentusers/upload-photo',
         },
         RECRUITER: {
-            LOGIN: '/api/employer/login',
-            REGISTER: '/api/employer/register',
+            LOGIN: '/api/employerusers/login',
+            REGISTER: '/api/employerusers/register',
+        },
+        ADMIN: {
+            LOGIN: '/api/admin/login',
+        }
+    },
+      ADMIN:{
+        USER:{
+            CREATE: '/api/admin/users',
+            GETALLUSERS: '/api/admin/users',
+            GETUSERBYID: (id: string) => `/api/admin/users/${id}`,
+            UPDATEUSERBYID: (id: string) => `/api/admin/users/${id}`,
+            DELETEUSERBYID: (id: string) => `/api/admin/users/${id}`,
         }
     }
 }
