4d95cf5dcbcf2323036c8d75cca3408b5367c411
yogesh25900 <chaudharyyogesh258@gmail.com>
2026-02-13 21:15:43 +0545

    update the api endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -7,10 +7,19 @@ export const API = {
             GETPROFILEBYID: '/api/talentusers',
             GETPROFILEMYSELF: '/api/talentusers/profile/me',
             UPLOADPHOTO: '/api/talentusers/upload-photo',
+            PASSWORDRESETOTP: '/api/talentusers/forgot-password',
+            VERIFYOTP: '/api/talentusers/verify-otp',
+            RESETPASSWORD: '/api/talentusers/reset-password',
         },
         RECRUITER: {
             LOGIN: '/api/employerusers/login',
             REGISTER: '/api/employerusers/register',
+            GETPROFILEBYID: '/api/employerusers',
+            EDITPROFILE: '/api/employerusers',
+            GETPROFILEMYSELF: '/api/employerusers/profile/me',
+            PASSWORDRESETOTP: '/api/employerusers/forgot-password',
+            VERIFYOTP: '/api/employerusers/verify-otp',
+            RESETPASSWORD: '/api/employerusers/reset-password',
         },
         ADMIN: {
             LOGIN: '/api/admin/login',
@@ -25,6 +34,10 @@ export const API = {
             DELETEUSERBYID: (id: string) => `/api/admin/users/${id}`,
         }
     },
+    CATEGORY: {
+        GETALL: '/api/categories',
+        GETBYID: (id: string) => `/api/categories/${id}`,
+    },
     JOB: {
         GETALLJOBS: '/api/jobs',
         GETJOBBYID: (id: string) => `/api/jobs/${id}`,
@@ -33,5 +46,18 @@ export const API = {
         UPDATEJOB: (id: string) => `/api/jobs/${id}`,
         DELETEJOB: (id: string) => `/api/jobs/${id}`,
         GETMYJOBS: '/api/jobs/employer/my-jobs',
+
+        VIEWSAVEDJOBS: '/api/saved-jobs/list',
+    },
+    JOB_APPLICATION: {
+        CREATE: '/api/applications',
+        GETALL: '/api/applications',
+        GET_BY_ID: (id: string) => `/api/applications/${id}`,
+        GETBYJOBID: (jobId: string) => `/api/applications/job/${jobId}`,
+        GETBYJOBID_WITH_SCORE: (jobId: string) => `/api/applications/job/${jobId}/with-score`,
+        GET_MY_APPLICATIONS: '/api/applications/talent/my-applications',
+        GETEMPLOYERAPPLICATIONS: '/api/applications/employer/applications',
+        UPDATESTATUS: (id: string) => `/api/applications/${id}/status`,
+        DELETE: (id: string) => `/api/applications/${id}`,
     }
 }
